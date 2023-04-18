4d24523dd4023c571b7de4c94f6211cf10fbbebb
pch-emeric <emeric.boyadjian@pch.click>
2023-04-18 18:15:00 +0200

    Setup postcss and clean app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import './globals.css'
 import Head from './Head'
+import Link from "next/link";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <Head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <Head/>
+        <header>
+            <nav>
+                <Link href="/">Accueil</Link>--
+                <Link href="/login">Connexion</Link>
+            </nav>
+        </header>
+        <body>{children}</body>
+        </html>
+    )
 }

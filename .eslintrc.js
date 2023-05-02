0b498d5945eb8282be8dbca47c761aef4f77b0ef
pch-emeric <emeric.boyadjian@pch.click>
2023-05-02 17:50:42 +0200

    Indent with the good format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true
-    },
-    extends: [
-      'plugin:vue/vue3-essential',
-      'standard-with-typescript'
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest'
-    },
-    plugins: [
-      'vue'
-    ],
-    rules: {
-    }
-  }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {}
+}

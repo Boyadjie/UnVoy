e4a9cf88d19acab03d3fc7673a0bd73a8e3ebd17
soulocean <Moreau.solo11@gmail.com>
2023-05-19 13:51:46 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
     env: {
       browser: true,
-      commonjs: true,
-      es2021: true
+      commonjs: true
     },
     extends: [
-      'plugin:vue/vue3-essential',
       'standard-with-typescript'
     ],
     overrides: [
     ],
-    parserOptions: {
-      ecmaVersion: 'latest'
-    },
     plugins: [
     ],
     rules: {

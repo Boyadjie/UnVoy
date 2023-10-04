2f43f55563f4235f821bc35a029d169a853659b4
Boyadjie <emericboy73@gmail.com>
2023-10-04 09:26:40 +0200

    remove problematic file

--- __test__/title.test.tsx
+++ __test__/title.test.tsx
@@ -1,10 +1,13 @@
 import {render, screen} from '@testing-library/react';
+
 import {Title} from '../src/components/title';
 
-it('Loads and display the atom "Title"', () => {
-	render(<Title />);
+describe('<Title />', () => {
+	it('Loads and display the atom "Title"', () => {
+		render(<Title />);
 
-	const title = screen.getByRole('heading');
+		const title = screen.getByRole('heading');
 
-	expect(title.textContent).toEqual('This is a title');
+		expect(title.textContent).toEqual('This is a title');
+	});
 });

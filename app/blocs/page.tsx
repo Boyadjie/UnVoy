ecd0eca90b01ba926eabb98ec818f3a15066107a
Boyadjie <emericboy73@gmail.com>
2023-10-17 15:05:23 +0200

    and blocList page styles

--- app/blocs/page.tsx
+++ app/blocs/page.tsx
@@ -1,24 +1,45 @@
-import {TextAndImg} from "../../src/components/Blocs/WithText/TextAndImg";
-import {TextBloc} from "../../src/components/Blocs/WithText/TextBloc";
-import styles from './page.module.css'
+import {TextAndImg} from '../../src/components/Blocs/WithText/TextAndImg';
+import {TextBloc} from '../../src/components/Blocs/WithText/TextBloc';
+import styles from './page.module.css';
 
 export default function Home() {
+	return (
+		<main className={styles.main}>
+			<h1>Liste des blocs</h1>
 
-    return (
-        <main className={styles.main}>
-            <h1>Liste des blocs</h1>
+			<section className={styles.blocListContainer}>
+				<div>
+					<h3>Text Bloc</h3>
+					<div>
+						<TextBloc content="This is a text" />
+						<TextBloc
+							content={['This is text 1', 'This is text 2', 'This is text 3']}
+						/>
+					</div>
+				</div>
 
-            <div>
-                <TextBloc content="This is a text"/>
-                <TextBloc content={["This is text 1", "This is text 2", "This is text 3"]}/>
-            </div>
-
-            <div>
-                <TextAndImg pictureDetails={{
-                    url: '/blocs/textAndImage.png',
-                    alt: 'Product picture'
-                }} textContent="This is a text with a picture"/>
-            </div>
-        </main>
-    )
+				<div>
+					<h3>Text And Image Bloc</h3>
+					<div>
+						<TextAndImg
+							pictureDetails={{
+								url: '/blocs/textAndImage.png',
+								alt: 'Product picture',
+							}}
+							textContent="This is a text with a picture"
+						/>
+						<TextAndImg
+							pictureDetails={{
+								url: '/blocs/textAndImage.png',
+								alt: 'Product picture',
+							}}
+							textContent="This is a text with a picture"
+							title="This is a title"
+							subtitle="This is a subtitle here to support the title"
+						/>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }

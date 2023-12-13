87e92fcd81e0eb862418494a85542d2cbf55bec6
Solo <Moreau.solo11@gmail.com>
2023-12-13 10:54:45 +0100

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,31 +1,27 @@
 export default function (req, res) {
-  let nodemailer = require('nodemailer');
-
-  // Configuration pour Mailtrap (remplacer avec vos propres crédentials)
+  let nodemailer = require('nodemailer')
   const transporter = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    port: 587,
+    host: "live.smtp.mailtrap.io",
     auth: {
-      user: 'votre_user_mailtrap', // Remplacer par votre user Mailtrap
-      pass: 'votre_mot_de_passe_mailtrap' // Remplacer par votre mot de passe Mailtrap
-    }
-  });
-
+      user: 'api',
+      pass: '5a6cab2e1752ab2a0fce78536b61575d',
+    },
+    secure: false,
+  })
   const mailData = {
-    from: 'demo@uncover-unvoy.com', // L'adresse expéditrice fictive
-    to: 'uncover.contact.us@gmail.com', // Adresse du destinataire
+    from: 'mailtrap@uncover.com',
+    to: 'uncover.contact.us@gmail.com',
     subject: `Message From ${req.body.name}`,
     text: req.body.message + " | Sent from: " + req.body.email,
-    html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`
   }
-
   transporter.sendMail(mailData, function (err, info) {
-    if(err) {
-      console.log(err);
-      res.status(500).send("Error sending email");
-    } else {
-      console.log(info);
-      res.status(200).send("Email sent successfully");
-    }
-  });
+    if(err)
+      console.log(err)
+    else
+      console.log(info)
+  })
+  res.status(200)
 }

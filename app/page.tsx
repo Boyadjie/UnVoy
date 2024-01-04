41aadcc7bafb4b0541c8b1139569808544d0729a
Solo <Moreau.solo11@gmail.com>
2024-01-04 15:48:55 +0100

    parnters part2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import {Header} from '../src/components/landing/header/Header';
-import {Story} from '../src/components/landing/story/Story';
 import {Partners} from '../src/components/landing/partners/Partners';
+import {Story} from '../src/components/landing/story/Story';
 import styles from './page.module.css';
 
 export default function Home() {

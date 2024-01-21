f043b62109089ad9c81e2c45d6fccb22b184551d
Solo <93206786+sologames7@users.noreply.github.com>
2024-01-21 23:34:31 +0100

    lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import {Contact} from '../src/components/landing/contact/Contact';
 import {Header} from '../src/components/landing/header/Header';
 import {Partners} from '../src/components/landing/partners/Partners';
 import {Product} from '../src/components/landing/product/Product';
 import {Separator} from '../src/components/landing/separator/Separator';
 import {Story} from '../src/components/landing/story/Story';
-import {Contact} from '../src/components/landing/contact/Contact';
-import { Team } from '../src/components/landing/team/Team';
+import {Team} from '../src/components/landing/team/Team';
 import styles from './page.module.css';
 
 export default function Home() {

4e0b059498111a4e6d07828676204ff9fa03f718
Solo <93206786+sologames7@users.noreply.github.com>
2024-01-21 23:34:31 +0100

    lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+import {Contact} from '../src/components/landing/contact/Contact';
 import {Header} from '../src/components/landing/header/Header';
 import {Story} from '../src/components/landing/story/Story';
-import {Contact} from '../src/components/landing/contact/Contact';
-import { Team } from '../src/components/landing/team/Team';
+import {Team} from '../src/components/landing/team/Team';
 import styles from './page.module.css';
 
 export default function Home() {
--- src/components/landing/team/Member.tsx
+++ src/components/landing/team/Member.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+
 import styles from './member.module.css';
 
 type MemberTypes = {

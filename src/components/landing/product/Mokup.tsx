495fe45b661cdc640392dbba6db424d2befdb6d8
Solo <155955412+moneymaker24@users.noreply.github.com>
2024-02-16 12:53:18 +0100

    Update mokup.module.css
    illustration fix & no hover on members

--- src/components/landing/product/Mokup.tsx
+++ src/components/landing/product/Mokup.tsx
@@ -1,16 +1,29 @@
 import Image from 'next/image';
 
 import {productData} from '../../../data/landingPageProduct';
+import {BubbleMiniBlue} from '../../decorations/BubbleMiniBlue';
+import {BubbleNineSquare} from '../../decorations/BubbleNineSquare';
+import styles from './mokup.module.css';
 
 export const Mokup = () => {
   const imgUrl = productData.mokup.imgUrl;
+
   return (
     // image from public folder
-    <Image
-      src={imgUrl}
-      alt="mokup"
-      width={400} // 1 because width is required
-      height={640} // 1 because height is required
-    />
+    <div className={styles.container}>
+      <div className={styles.bubbleSquare}>
+        <BubbleNineSquare />
+      </div>
+      <div className={styles.bubbleMiniBlue}>
+        <BubbleMiniBlue />
+      </div>
+      <Image
+        src={imgUrl}
+        alt="mokup"
+        width={400} // 1 because width is required
+        height={640} // 1 because height is required
+        className={styles.mokupImage}
+      />
+    </div>
   );
 };

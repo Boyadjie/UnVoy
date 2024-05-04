2760164ac4c1f7cb5c87bd83f461c565bd1a2002
Boyadjie <emericboy73@gmail.com>
2024-05-04 15:12:11 +0200

    fix(#47): refacto map filters

--- src/components/map/MapPreview.tsx
+++ src/components/map/MapPreview.tsx
@@ -3,63 +3,43 @@ import Link from 'next/link';
 
 import styles from './mapPreview.module.css';
 
+const filtersImageUrls = [
+  '/icons/map/filter/map_restaurant.svg',
+  '/icons/map/filter/map_cle.svg',
+  '/icons/map/filter/map_coeur.svg',
+  '/icons/map/filter/map_point_interrogation.svg',
+  '/icons/map/filter/map_localisation.svg',
+  '/icons/map/filter/map_appareil_photo.svg',
+  '/icons/map/filter/map_supermarche.svg',
+];
+
 export const MapPreview = () => {
   return (
     <div className={styles.previewContainer}>
       <h4>Se repérer au Japon</h4>
 
-      <Link href="#" className={styles.mapContainer}>
-        <Image
-          src="/illustrations/map.svg"
-          width={315}
-          height={200}
-          alt="Carte"
-        />
+      <div className={styles.mapContainer}>
+        <Link href="#">
+          <Image
+            src="/illustrations/map.svg"
+            width={315}
+            height={200}
+            alt="Carte"
+          />
+        </Link>
         <div className={styles.filters}>
-          <Image
-            src="/icons/map/filter/map_restaurant.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_cle.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_coeur.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_point_interrogation.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_localisation.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_appareil_photo.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
-          <Image
-            src="/icons/map/filter/map_supermarche.svg"
-            alt="filtre"
-            width={25}
-            height={25}
-          />
+          {filtersImageUrls.map((url, id) => (
+            <Image
+              key={id}
+              className={styles.filter}
+              src={url}
+              alt="filtre"
+              width={25}
+              height={25}
+            />
+          ))}
         </div>
-      </Link>
+      </div>
     </div>
   );
 };

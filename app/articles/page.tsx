2f1a41079b41b552581850f72aceda58e732b06a
Boyadjie <emericboy73@gmail.com>
2024-06-29 18:41:50 +0200

    feat(#60): complete articles list page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,14 +1,51 @@
 'use client';
+import {useState} from 'react';
+
+import {ArticlesList} from '../../src/components/articles/articlesList/ArticlesList';
+import {InputRadio} from '../../src/components/Form/Inputs/InputRadio';
+import {Navbar} from '../../src/components/navbar/Navbar';
 import {useArticles} from '../../src/hook/useArticles';
+import styles from './page.module.css';
+
+type CategoryName = 'decouverte' | 'intermediaire' | 'expert';
+type CategoriesInput = {
+  label: string;
+  value: CategoryName;
+}[];
 
 export default function Articles() {
   const {categories, loading} = useArticles();
+  const categoriesInput: CategoriesInput = [
+    {label: 'Découverte', value: 'decouverte'},
+    {label: 'Intermédiaire', value: 'intermediaire'},
+    {label: 'Expert', value: 'expert'},
+  ];
+  const [selectedCategory, setSelectedCategory] =
+    useState<CategoryName>('decouverte');
 
   return (
     <>
-      <h1>Articles</h1>
-      {loading && 'Loading ...'}
-      {!loading && <pre>{JSON.stringify(categories)}</pre>}
+      <Navbar />
+      <div className={styles.articleListContainer}>
+        <h1>ARTICLES</h1>
+        <div className={styles.selectCategory}>
+          {categoriesInput.map(({label, value}, index) => (
+            <InputRadio
+              key={index}
+              label={label}
+              name="category"
+              value={value}
+              defaultValue={selectedCategory}
+              handleChange={() => setSelectedCategory(value)}
+            />
+          ))}
+        </div>
+
+        {loading && 'Loading ...'}
+        {!loading && categories && (
+          <ArticlesList articles={categories[selectedCategory]} />
+        )}
+      </div>
     </>
   );
 }
--- src/components/blocs/preview/article/ArticlePreview.tsx
+++ src/components/blocs/preview/article/ArticlePreview.tsx
@@ -8,18 +8,31 @@ export const ArticlePreview: React.FC<ArticleProps> = ({
   pictureUrl,
   title,
   date,
+  fullWidth,
+  rate,
 }) => {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${fullWidth ? styles.fullWidth : ''}`}>
       <div className={styles.imageContainer}>
         <Image src={pictureUrl} alt={title} width={125} height={125} />
+        {rate && (
+          <span className={styles.rate}>
+            <Image
+              src="/icons/map/recommendations/like_noir.svg"
+              alt="note"
+              width={10}
+              height={10}
+            />{' '}
+            {rate * 100}%
+          </span>
+        )}
         <button>
           <Image src="/icons/coeur.svg" alt="coeur" width={17} height={17} />
         </button>
       </div>
       <div className={styles.details}>
         <h3>{title}</h3>
-        <p>{format(date, 'yyyy-MM-dd')}</p>
+        <p>{format(date, 'd MMM. yyyy')}</p>
       </div>
     </div>
   );

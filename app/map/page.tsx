644ac1901ac7efe5c22f1c4a6053f02e0200b665
Solo <93206786+sologames7@users.noreply.github.com>
2024-07-04 21:54:26 +0200

    map v1

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import {useState} from 'react';
+
 import dynamic from 'next/dynamic';
-import Image from 'next/image';
 
 import {Navbar} from '../../src/components/navbar/Navbar';
 import styles from './page.module.css';
@@ -11,40 +12,99 @@ const DynamicMapComponent = dynamic(
 );
 
 export default function Map() {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [submittedQuestion, setSubmittedQuestion] = useState<string | null>(
+    null,
+  );
+  const [markerData, setMarkerData] = useState<MarkerData | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      setSubmittedQuestion(inputValue);
+      setInputValue('');
+
+      const response = await fetch('/api/coordinates', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({value: inputValue}),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      let data;
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        data = await response.json();
+      } else {
+        const text = await response.text();
+        console.error('Received non-JSON response:', text);
+        throw new Error('Received non-JSON response');
+      }
+
+      setMarkerData(data);
+    } catch (error) {
+      console.error('Error in handleSubmit:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <div id={styles.map}>
         <Navbar />
         <section className={styles.layout}>
           <div className={styles.layoutTop}>
-            <div className={styles.layoutTitles}>
-              <h3 className={styles.layoutTitle}>MAP</h3>
-              <p className={styles.layoutSubTitle}>
-                {'Logement vers aeroport'}
-              </p>
-            </div>
             <div className={styles.layoutMenu}>
-              <button className={styles.layoutButton}>
-                <Image
-                  src="/icons/map/layout/map_filter.svg"
-                  alt="Icône de filtre"
-                  width={25}
-                  height={20}
-                />
-              </button>
-              <button className={styles.layoutButton}>
-                <Image
-                  src="/icons/map/layout/map_path.svg"
-                  alt="Icône de chemin"
-                  width={25}
-                  height={20}
-                />
-              </button>
+              <div>
+                <div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      width: '100vw',
+                      gap: '10px',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <input
+                      type="text"
+                      value={inputValue}
+                      onChange={(e) => setInputValue(e.target.value)}
+                      className={styles.input}
+                      onKeyPress={(e) => {
+                        if (e.key === 'Enter') handleSubmit();
+                      }}
+                    />
+                    <button onClick={handleSubmit} className={styles.goButton}>
+                      Search
+                    </button>
+                  </div>
+                  {submittedQuestion && (
+                    <div>
+                      <p>Results for: "{submittedQuestion}"</p>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </section>
         <div className={styles.mapContentContainer}>
-          <DynamicMapComponent />
+          <DynamicMapComponent
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            submittedQuestion={submittedQuestion}
+            setSubmittedQuestion={setSubmittedQuestion}
+            markerData={markerData}
+            setMarkerData={setMarkerData}
+            loading={loading}
+            setLoading={setLoading}
+          />
         </div>
       </div>
     </>
